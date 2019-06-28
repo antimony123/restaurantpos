c506d0f27c2b2be76ae23ebcd8bf9706181a06c0
antimony123 <Benten@refracta>
2019-06-28 23:32:37 +0000

    updated Sean's menu code to work with db.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
 
-import mysql.connector as mc
+import db
 
 app = Flask(__name__)
+resdb = db.RestaurantDB("localhost")
 
 @app.route("/")
 def main():
@@ -22,15 +23,48 @@ def verifylogin():
 		print("ERROR!!")
 	return render_template('index.html')
 
-@app.route("/inventory")
-def show_inventory():
-	mydb = mc.connect(host='localhost', user='webaccess', passwd='cs160mysql', database="RESMGTDB")
-	cur = mydb.cursor()
-	cur.execute("DESCRIBE ingredients;")
-	fields = [row[0] for row in cur]
-	cur.execute("SELECT * FROM ingredients;")
-	data = [row for row in cur]
-	return render_template('inventory.html', fields=fields, data=data)
+@app.route("/menu_mgt")
+def main_menu():
+        return render_template("menu_mgt.html")
+
+@app.route("/menu_add")
+def add_item():
+        return render_template("menu_add.html")
+
+@app.route("/add_review", methods=["POST", "GET"])
+def add_review():
+        if request.method == "POST" :
+	#TODO: implement error handling
+                if not request.form["item_name"] or not request.form["item_price"] or not request.form["item_category"] :
+                        return render_template("menu_add.html")
+                return render_template("add_review.html", item_name=request.form["item_name"], \
+                        item_price=request.form["item_price"], item_category=request.form["item_category"] )
+
+@app.route("/add_final", methods=["POST", "GET"])
+def insert_new_menu_item():
+	if request.method == "POST" :
+
+		entry = (request.form["item_name"], \
+			request.form["item_price"], \
+			request.form["item_category"])
+
+		resdb.create_entry("menu", entry)
+
+		return render_template("add_final.html", \
+					fields=resdb.get_field_names("menu"), \
+					data=resdb.get_table_contents("menu"))
+
+@app.route("/menu_del")
+def select_to_delete():
+	return render_template("menu_del.html", \
+			fields=resdb.get_field_names("menu"), \
+			data=resdb.get_table_contents("menu"))
+@app.route("/del_review", methods=["POST", "GET"])
+def delete_review():
+	if request.method == "POST":
+		print([i for i in request.form.keys()])
+	return render_template("del_review.html")
+
 
 if __name__ == "__main__":
     app.run()
