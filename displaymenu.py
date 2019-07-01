4699f00d36bc1d8c48e1872acedba2dd3d7b42b0
fd11kt <52010448+fd11kt@users.noreply.github.com>
2019-06-30 21:55:45 -0700

    Update displaymenu.py
    Fixed column widths to match
    Updated code

--- displaymenu.py
+++ displaymenu.py
@@ -16,7 +16,7 @@ my.setcontent()
 
 #from mysql.connector import (errorcode) 
 try:
-    cnx = mysql.connector.connect(user='usernamehere', password='passwordhere', host='localhostIP', database='databasehere')
+    cnx = mysql.connector.connect(user='usernameHere', password='pwHere', host='localhostIP', database='databaseName')
 
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -34,17 +34,8 @@ finally:
     qs=os.environ['QUERY_STRING']
     guestname=my.parse_query(qs,'guestname')
     orderid=my.parse_query(qs,'orderid')
-    query = "INSERT INTO order_temp (guestname, orderid) VALUES (%s,%s)"
-#    print(query)
-    args = (guestname, orderid)
-    cur.execute(query,args)
-    cnx.commit()
 
 #---Present Menu -------------------------------------------------------------------------------------------
-
-    query = ("SELECT * from menu WHERE category='Food'")
-#    print(query)
-    cur.execute(query)
 
     my.starthtml()
     my.startheader()
@@ -53,47 +44,56 @@ finally:
     print("<style> table, th, td { border: 1px solid black;</style>")
 
     my.startbody()
-
     my.startform('customizeorder.py')
 
-#    print("<form action=\"customizeorder.py\">\r\n")
+#---------------------------------------------------------------------------------------------------------------
 
+    query = ("SELECT * from menu WHERE category='Food'")
+    cur.execute(query)
+
+    print("<table>")
+    print("<col width=\"400\"><col width=\"30\">")
     print()
     print("<h3>Main Dishes</h3>")
-    print("<table style=\"width:100%\">")
-    print("<tr><td><h4>Description</h4></td><td><h4>Price</h4></td>") #<td><h4>Quantity</h4></td></tr>")
+    print("<tr><td><h4>Description</h4></td><td><h4>Price</h4></td>")
     for r in cur:
         print("<tr>")
-        print("<td><input type=\"radio\" name=\"mainorder\" value=",r[0],">",r[1],"</td>","<td>",r[2],"</td>")  #<td><input type='text' quantity_main='quantmain'></td>")
+        print("<td><input type=\"radio\" name=\"mainorder\" value=",r[0],">",r[1],"</td>","<td>",r[2],"</td>")
         print("</tr>")
     print("</table>")
+
 #---------------------------------------------------------------------------------------------------------------
+
     query = ("SELECT * from menu WHERE category='Side'")
     cur.execute(query)
-    print("<table style=\"width:100%\">")
+    print("<table>")
+    print("<col width=\"400\"><col width=\"30\">")
     print()
     print("<h3>Side Dishes</h3>")
-    print("<tr><td><h4>Description</h4></td><td><h4>Price</h4></td>") #<td><h4>Quantity</h4></td></tr>")
+    print("<tr><td><h4>Description</h4></td><td><h4>Price</h4></td>")
     for r in cur:
         print("<tr>")
-        print("<td><input type=\"radio\" name=\"sideorder\" value=",r[0],">",r[1],"</td>","<td>",r[2],"</td>")  #<td><input type='text' quantity_side='quantside'></td>")
+        print("<td><input type=\"radio\" name=\"sideorder\" value=",r[0],">",r[1],"</td>","<td>",r[2],"</td>")
         print("</tr>")
     print("</table>")
+
 #---------------------------------------------------------------------------------------------------------------
     query = ("SELECT * from menu WHERE category='Drink'")
     cur.execute(query)
-    print("<table style=\"width:100%\">")
+    print("<table>")
+    print("<col width=\"400\"><col width=\"30\">")
     print()
     print("<h3>Drinks</h3>")
-    print("<tr><td><h4>Description</h4></td><td><h4>Price</h4></td>") #<td><h4>Quantity</h4></td></tr>")
+    print("<tr><td><h4>Description</h4></td><td><h4>Price</h4></td>")
     for r in cur:
         print("<tr>")
-        print("<td><input type=\"radio\" name=\"drinkorder\" value=",r[0],">",r[1],"</td>","<td>",r[2],"</td>")  #<td><input type='text' quantity_drink='quantdrink'></td>")
+        print("<td><input type=\"radio\" name=\"drinkorder\" value=",r[0],">",r[1],"</td>","<td>",r[2],"</td>")
         print("</tr>")
     print("</table>")
-
+#---------------------------------------------------------------------------------------------------------------
     print("<input type='hidden' name='guestname' value=", guestname, "><br><br>")
     print("<input type='hidden' name='orderid' value=", orderid,"><br><br>")
+
     print("<br><br><input type=\"submit\" value=\"Customize your Order\">")
 
     my.closeform()
