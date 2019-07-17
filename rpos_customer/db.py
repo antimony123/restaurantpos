c0ff4d89ba838e5958b35ae1889aac3c0bcf751c
fd11kt <52010448+fd11kt@users.noreply.github.com>
2019-07-17 14:17:20 -0700

    Update db.py

--- rpos_customer/db.py
+++ rpos_customer/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('mysql+pymysql://root:12345678@localhost:3306/RESMGTDB') #, convert_unicode=True)
+engine = create_engine('mysql+pymysql://root:putpwhere@localhost:3306/RESMGTDB') #, convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
